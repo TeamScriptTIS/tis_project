d193a6dbdfff79158867ff3e2cff00edb5e7efcb
Sergio Rodrigo Cardenas Rivera <rodrigosergio93@gmail.com>
2015-05-17 12:31:57 -0400

    arreglo
    123456

--- conexion/conexion.php
+++ conexion/conexion.php
@@ -1,5 +1,4 @@
 <?php
-/*CONEXION CON LA BASE DE DATOS USUARIO:jimmy PASS:201101027 BASEDEDATOS:sistema_tis*/
 	$conn = mysql_connect("localhost","root","") or die("Error en conexion");
 	mysql_select_db("tis_munisoft", $conn) or die("Error en base de datos");
 	mysql_query("SET NAMES 'utf8'");
